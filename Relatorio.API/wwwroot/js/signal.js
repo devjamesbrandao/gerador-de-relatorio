fa825a6101827b6338ed5a6e52f7cf386e8561b9
James Noleto <muricijames@gmail.com>
2022-09-08 22:27:17 -0300

    feat: alterando o status do relatório com Javascript

--- Relatorio.API/wwwroot/js/signal.js
+++ Relatorio.API/wwwroot/js/signal.js
@@ -2,8 +2,10 @@
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/relatorioHub").build();
 
-connection.on("ReceiveMessage", function (message) {
-    console.log(message);
+connection.on("ReceiveMessage", function (id) {
+    document.getElementById(`relatorio-item-color-${id}`).className = "btn btn-success disabled";
+
+    document.getElementById(`relatorio-item-status-${id}`).innerHTML = "CONCLUÍDO";
 });
 
 connection.start().then(function () {
