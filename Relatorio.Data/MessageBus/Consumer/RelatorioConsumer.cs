ad347596a55194718d050faa05f3603c276a4a9b
James Noleto <muricijames@gmail.com>
2022-09-08 22:29:30 -0300

    feat: consumer rabbitMQ para processar os relatórios solicitados

---
+++ Relatorio.Data/MessageBus/Consumer/RelatorioConsumer.cs
@@ -0,0 +1,82 @@
+using System.Text;
+using System.Text.Json;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Relatorio.Data.Context;
+using Relatorio.Data.Hubs;
+
+namespace Relatorio.Data.MessageBus.Consumer
+{
+    public class RelatorioConsumer : BackgroundService
+    {
+        private const string QUEUE = "Relatorios";
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+        private readonly IServiceProvider _serviceProvider;
+        private readonly IHubContext<RelatorioHub> _hub;
+
+        public RelatorioConsumer(IServiceProvider serviceProvider, IHubContext<RelatorioHub> hub)
+        {
+            _serviceProvider = serviceProvider;
+
+            _hub = hub;
+
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+            
+            _connection = factory.CreateConnection();
+
+            _channel = _connection.CreateModel();
+
+            _channel.QueueDeclare(
+                queue: QUEUE,
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+            );
+        }
+        
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var consumer = new EventingBasicConsumer(_channel);
+
+            consumer.Received += (sender, eventArgs) =>
+            {
+                var byteArray = eventArgs.Body.ToArray();
+                
+                var relatorioString = Encoding.UTF8.GetString(byteArray);
+
+                var relatorio = JsonSerializer.Deserialize<Relatorio.Core.Models.Relatorio>(relatorioString);
+
+                _channel.BasicAck(eventArgs.DeliveryTag, false);
+
+                ProcessarRelatorio(relatorio).GetAwaiter().GetResult();
+            };
+
+            _channel.BasicConsume(QUEUE, false, consumer);
+
+            return Task.CompletedTask;
+        }
+
+        public async Task ProcessarRelatorio(Relatorio.Core.Models.Relatorio relatorio)
+        {
+            await Task.Delay(relatorio.Duracao * 1000);   
+
+            using(var scope = _serviceProvider.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+                context.Database.ExecuteSqlRaw("update Relatorios set Concluido = 1 where Id = {0}", relatorio.Id);
+            }
+
+            await _hub.Clients.All.SendAsync("ReceiveMessage", relatorio.Id);
+        }
+    }
+}
