8063a59cbcd052a9b106b3949ef97466987912ed
James Noleto <muricijames@gmail.com>
2022-09-09 22:13:34 -0300

    feat: configurando Health Checks do Sql Server e RabbitMQ

--- Relatorio.API/Startup.cs
+++ Relatorio.API/Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using DevFreela.infrastructure.MessageBus;
+using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -42,6 +44,21 @@ namespace Relatorio
                     )
                     .LogTo(Console.WriteLine)
             );
+
+            //Add Sql Server Health Check 
+            services.AddHealthChecks()
+            .AddSqlServer(
+                connectionString: connectionString, 
+                name: "Sql Server instance",
+                tags: new[] { "database" }
+            ).AddRabbitMQ(
+                rabbitConnectionString: Configuration.GetConnectionString("RabbitMQ"), 
+                name: "rabbitMQ",
+                tags: new[] { "message-broker"}
+            );
+            
+            // Add Health Check UI
+            services.AddHealthChecksUI().AddInMemoryStorage();
 
             services.AddControllersWithViews();
 
@@ -88,12 +105,24 @@ namespace Relatorio
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapHealthChecksUI();
+
+                //Route to all Health Checks
+                endpoints.MapHealthChecks("/health", new HealthCheckOptions
+                {
+                    Predicate = p => true,
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
+
                 endpoints.MapHub<RelatorioHub>("/relatorioHub");
 
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            //Route to acess health check UI
+            app.UseHealthChecksUI(options => { options.UIPath = "/dashboard"; });
         }
     }
 }
